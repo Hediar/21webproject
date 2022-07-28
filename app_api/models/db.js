674d17c4a33a725c1a90525cccb873c0dd33e64c
Hediar <72387948+Hediar@users.noreply.github.com>
2022-07-28 14:25:06 +0900

    Update db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //const dbURI = 'mongodb://localhost/Loc8r';
-const dbURI = 'mongodb+srv://my_atlas_user:@cluster0.4ytaf.mongodb.net/Loc8r';
+const dbURI = 'mongodb+srv://my_atlas_user:<password>@cluster0.4ytaf.mongodb.net/<dbname>';
 
 mongoose.connect(dbURI, {useNewUrlParser: true});
 mongoose.connection.on('connected', () => {
